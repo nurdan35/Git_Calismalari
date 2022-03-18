b8105e9d87893e41aaad3868346c86ab98dce5b5
Nurdan Kurcak <you@example.com>
2022-03-18 18:54:18 +0100

    HelloGit updated, nurdan1234 test file uploaded

--- src/test/java/day_1/HelloGit.java
+++ src/test/java/day_1/HelloGit.java
@@ -5,5 +5,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("2. satir calismasi");
+
+        // yeni test kodu ile ilgili eklemeler yaptim!!
     }
 }
